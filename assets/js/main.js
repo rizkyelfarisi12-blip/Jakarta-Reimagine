4045cf23f8a17ead474a6a42a11ba2d8649ce11f
rizkyelfarisi12-blip <rizkyelfarisi12@gmail.com>
2026-07-02 17:12:59 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+// Navbar scroll effect
+const header = document.getElementById("header");
+
+window.addEventListener("scroll", () => {
+
+    if(window.scrollY > 80){
+
+        header.classList.add("scrolled");
+
+    }else{
+
+        header.classList.remove("scrolled");
+
+    }
+
+});
+
+
+// Navigation button click handlers
+navButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        navButtons.forEach(btn => btn.classList.remove('active'));
+        button.classList.add('active');
+    });
+});
